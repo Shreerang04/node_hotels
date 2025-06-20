872cdb33de9d7e88b52bc3814ff5cc93fb08f155
Shreerang Supekar <shreerangsupekar04@gmail.com>
2025-06-20 14:39:34 +0530

    2nd version of node_hotels

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURL ='mongodb://localhost:27017/hotels'
+require('dotenv').config();
+
+//const mongoURL ='mongodb://localhost:27017/hotels'
+const mongoURL = process.env.MONGODB_URL;
 
 
 mongoose.connect(mongoURL)
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express=require('express')
+require('dotenv').config();
 const app = express();
 const db= require('./db');
+
+const PORT =process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());// store jata in req.body
@@ -22,4 +25,4 @@ app.use('/menu', menuItemRoutes);
 
 
 
-app.listen(3000)
+app.listen(PORT)
